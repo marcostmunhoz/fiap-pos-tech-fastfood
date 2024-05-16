b4ed2ff095ea6343869184b39407aeda4285ccb0
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-16 19:35:46 -0300

    test(customer): fix broken CustomerEntity test

--- src/customer/domain/entity/customer.entity.spec.ts
+++ src/customer/domain/entity/customer.entity.spec.ts
@@ -1,7 +1,26 @@
-import { CustomerEntity } from './customer.entity';
+import { EntityIdValueObject } from '@/shared/domain/value-object/entity-id.value-object';
+import { FullNameValueObject } from '@/customer/domain/value-object/full-name.value-object';
+import { CpfValueObject } from '@/customer/domain/value-object/cpf.value-object';
+import { EmailValueObject } from '@/customer/domain/value-object/email.value-object';
+import { CustomerEntity, CustomerEntityPropsWithId } from './customer.entity';
 
-describe('Customer', () => {
-  it('should be defined', () => {
-    expect(new CustomerEntity()).toBeDefined();
+describe('CustomerEntity', () => {
+  describe('create', () => {
+    it('should return a new Customer instance', async () => {
+      // Arrange
+      const props: CustomerEntityPropsWithId = {
+        id: EntityIdValueObject.create('some-id'),
+        fullName: FullNameValueObject.createFromFullName('John Doe'),
+        cpf: CpfValueObject.create('57516713090'),
+        email: EmailValueObject.create('john.doe@example.com'),
+      };
+
+      // Act
+      const result = CustomerEntity.create(props);
+
+      // Assert
+      expect(result).toBeInstanceOf(CustomerEntity);
+      expect(result).toEqual(props);
+    });
   });
 });
