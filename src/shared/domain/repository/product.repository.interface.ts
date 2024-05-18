397b00ee4f976959af12b9644060c622c8cea9c4
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-18 14:54:06 -0300

    fix(shared): replace Customer references for Product in ProductRepository interface

--- src/shared/domain/repository/product.repository.interface.ts
+++ src/shared/domain/repository/product.repository.interface.ts
@@ -1,15 +1,22 @@
-import { CustomerEntity } from '@/customer/domain/entity/customer.entity';
 import {
   EssentialProductEntityProps,
   ProductEntity,
 } from '../entity/product.entity';
+import { ProductCategoryEnum } from '../enum/product-category.enum';
 import { EntityIdValueObject } from '../value-object/entity-id.value-object';
+import { ProductCodeValueObject } from '../value-object/product-code.value-object';
+
+export type SearchProductQuery = {
+  query?: string;
+  category?: ProductCategoryEnum;
+};
 
 export interface ProductRepository {
   list(): Promise<ProductEntity[]>;
-  search(query: string): Promise<ProductEntity[]>;
+  search(filter: SearchProductQuery): Promise<ProductEntity[]>;
   findById(id: EntityIdValueObject): Promise<ProductEntity | null>;
-  create(product: EssentialProductEntityProps): Promise<CustomerEntity>;
-  update(product: ProductEntity): Promise<CustomerEntity>;
+  create(product: EssentialProductEntityProps): Promise<ProductEntity>;
+  update(product: ProductEntity): Promise<ProductEntity>;
   delete(id: EntityIdValueObject): Promise<void>;
+  existsWithCode(code: ProductCodeValueObject): Promise<boolean>;
 }
