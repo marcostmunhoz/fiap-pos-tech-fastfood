c11595c52e1dad46d450df94b3aa06c505b19389
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-18 14:54:32 -0300

    refactor(shared): change MoneyValueObject to work with integer values

--- src/shared/domain/value-object/money.value-object.spec.ts
+++ src/shared/domain/value-object/money.value-object.spec.ts
@@ -4,7 +4,7 @@ describe('MoneyValueObject', () => {
   describe('create', () => {
     it('should create a MoneyValueObject from a valid value', () => {
       // Arrange
-      const value = 10.5;
+      const value = 100;
 
       // Act
       const instance = MoneyValueObject.create(value);
@@ -15,7 +15,7 @@ describe('MoneyValueObject', () => {
 
     it('should throw an error when the value is negative', () => {
       // Arrange
-      const negativeValue = -10.5;
+      const negativeValue = -100;
 
       // Act
       const create = () => MoneyValueObject.create(negativeValue);
@@ -35,12 +35,58 @@ describe('MoneyValueObject', () => {
       expect(create).toThrow('Invalid money value.');
     });
 
-    it('should throw an error when the value is lesser than the minimum value', () => {
+    it('should throw an error when the value is NaN', () => {
       // Arrange
-      const value = 0.001;
+      const nanValue = NaN;
 
       // Act
-      const create = () => MoneyValueObject.create(value);
+      const create = () => MoneyValueObject.create(nanValue);
+
+      // Act & Assert
+      expect(create).toThrow('Invalid money value.');
+    });
+
+    it('should throw an error when the value is a float', () => {
+      // Arrange
+      const float = 100.1;
+
+      // Act
+      const create = () => MoneyValueObject.create(float);
+
+      // Act & Assert
+      expect(create).toThrow('Invalid money value.');
+    });
+  });
+
+  describe('createFromFloat', () => {
+    it('should create a MoneyValueObject from a valid float value', () => {
+      // Arrange
+      const value = 1.0;
+
+      // Act
+      const instance = MoneyValueObject.createFromFloat(value);
+
+      // Assert
+      expect(instance.value).toBe(100);
+    });
+
+    it('should throw an error when the value is negative', () => {
+      // Arrange
+      const negativeValue = -1.0;
+
+      // Act
+      const create = () => MoneyValueObject.createFromFloat(negativeValue);
+
+      // Act & Assert
+      expect(create).toThrow('Invalid money value.');
+    });
+
+    it('should throw an error when the value is zero', () => {
+      // Arrange
+      const zeroValue = 0.0;
+
+      // Act
+      const create = () => MoneyValueObject.createFromFloat(zeroValue);
 
       // Act & Assert
       expect(create).toThrow('Invalid money value.');
@@ -51,7 +97,7 @@ describe('MoneyValueObject', () => {
       const nanValue = NaN;
 
       // Act
-      const create = () => MoneyValueObject.create(nanValue);
+      const create = () => MoneyValueObject.createFromFloat(nanValue);
 
       // Act & Assert
       expect(create).toThrow('Invalid money value.');
--- src/shared/domain/value-object/money.value-object.ts
+++ src/shared/domain/value-object/money.value-object.ts
@@ -9,6 +9,13 @@ export class MoneyValueObject extends AbstractValueObject<MoneyValueObjectProps>
     return this.props.value;
   }
 
+  get valueAsFloat(): number {
+    return parseFloat((this.value / 100).toFixed(2));
+  }
+
+  /**
+   * @param {number} value an integer representation (in cents) of the money value
+   */
   public static create(value: number): MoneyValueObject {
     if (!this.isValid(value)) {
       this.throwInvalidValue('Invalid money value.');
@@ -17,12 +24,20 @@ export class MoneyValueObject extends AbstractValueObject<MoneyValueObjectProps>
     return new MoneyValueObject({ value });
   }
 
+  public static createFromFloat(value: number): MoneyValueObject {
+    return this.create(Math.round(value * 100));
+  }
+
   private static isValid(value: number): boolean {
+    if (value % 1 !== 0) {
+      return false;
+    }
+
     if (value === null || value === undefined || isNaN(value)) {
       return false;
     }
 
-    if (value < 0.01) {
+    if (value < 1) {
       return false;
     }
 
