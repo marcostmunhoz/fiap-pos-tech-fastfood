f15aab75ef6399f55426bc4844e39ddd44690a8e
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-22 21:16:02 -0300

    feat(payment): add FakePaymentGatewayService

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PaymentFactory } from './domain/factory/payment.factory';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentEntity } from './infrastructure/entity/payment.entity';
-import { PaymentRepositoryToken } from './tokens';
+import { PaymentGatewayServiceToken, PaymentRepositoryToken } from './tokens';
 import { TypeOrmPaymentRepository } from './infrastructure/repository/type-orm-payment.repository';
 import { CreatePaymentController } from './interface/controller/create-payment.controller';
 import { CreatePaymentUseCase } from './application/use-case/create-payment.use-case';
+import { FakePaymentGatewayService } from './infrastructure/service/fake-payment-gateway.service';
 
 const factories = [PaymentFactory];
 const useCases = [CreatePaymentUseCase];
@@ -17,6 +18,10 @@ const useCases = [CreatePaymentUseCase];
     {
       provide: PaymentRepositoryToken,
       useClass: TypeOrmPaymentRepository,
+    },
+    {
+      provide: PaymentGatewayServiceToken,
+      useClass: FakePaymentGatewayService,
     },
     ...factories,
     ...useCases,
