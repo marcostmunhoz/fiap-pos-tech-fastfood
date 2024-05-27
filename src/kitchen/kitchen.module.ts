47a2139c8e61713e8d30face1463e28a160ebb1c
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-26 23:36:51 -0300

    chore: ignore healthcheck endpoint from exposed API docs

--- src/kitchen/kitchen.module.ts
+++ src/kitchen/kitchen.module.ts
@@ -2,11 +2,13 @@ import { SharedModule } from '@/shared/shared.module';
 import { Module } from '@nestjs/common';
 import { CreateProductUseCase } from './application/use-case/create-product.use-case';
 import { DeleteProductUseCase } from './application/use-case/delete-product.use-case';
+import { ListOrdersUseCase } from './application/use-case/list-orders.use-case';
 import { SearchProductsUseCase } from './application/use-case/search-products.use-case';
 import { ShowProductUseCase } from './application/use-case/show-product.use-case';
 import { UpdateProductUseCase } from './application/use-case/update-product.use-case';
 import { CreateProductController } from './interface/controller/create-product.controller';
 import { DeleteProductController } from './interface/controller/delete-product.controller';
+import { ListOrdersController } from './interface/controller/list-orders.controller';
 import { SearchProductsController } from './interface/controller/search-products.controller';
 import { ShowProductController } from './interface/controller/show-product.controller';
 import { UpdateProductController } from './interface/controller/update-product.controller';
@@ -17,6 +19,7 @@ const useCases = [
   UpdateProductUseCase,
   DeleteProductUseCase,
   SearchProductsUseCase,
+  ListOrdersUseCase,
 ];
 
 @Module({
@@ -28,6 +31,7 @@ const useCases = [
     UpdateProductController,
     DeleteProductController,
     SearchProductsController,
+    ListOrdersController,
   ],
 })
 export class KitchenModule {}
