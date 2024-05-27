002f70a8beb60e2eb3c77c4e1fac8d3b8bc16080
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-05-26 23:53:03 -0300

    chore: ignore healthcheck endpoint from exposed API docs

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import {
   HealthCheck,
   HealthCheckService,
@@ -7,6 +8,7 @@ import {
 } from '@nestjs/terminus';
 
 @Controller('health')
+@ApiExcludeController()
 export class HealthController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
