0b4e909193ec2df31b3084e535d832713abe79b0
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-07-14 16:46:40 -0300

    chore: add load test endpoint

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -2,9 +2,10 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
+import { LoadController } from './load.controller';
 
 @Module({
   imports: [TerminusModule, HttpModule],
-  controllers: [HealthController],
+  controllers: [HealthController, LoadController],
 })
 export class HealthModule {}
---
+++ src/health/load.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+
+@Controller('load')
+export class LoadController {
+  @Get('/')
+  @HttpCode(200)
+  async execute(@Query('level') level: number = 50): Promise<string> {
+    const n = Math.min(level, 50);
+
+    return `fib(${n}): ${this.fibonacci(n)}`;
+  }
+
+  private fibonacci(n: number): number {
+    if (n <= 1) {
+      return n;
+    }
+
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  }
+}
